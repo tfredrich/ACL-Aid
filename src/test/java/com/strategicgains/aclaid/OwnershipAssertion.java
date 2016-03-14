973c119473f9f047cf00ec07867c551203e5ef67
toddf <tfredrich@gmail.com>
2016-03-13 23:32:08 -0600

    Introduced AssertionTest to test ACL with assertions.

---
+++ src/test/java/com/strategicgains/aclaid/OwnershipAssertion.java
@@ -0,0 +1,46 @@
+/*
+    Copyright 2016, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+package com.strategicgains.aclaid;
+
+import com.strategicgains.aclaid.impl.RoleImpl;
+
+/**
+ * @author toddf
+ * @since Mar 13, 2016
+ */
+public class OwnershipAssertion
+implements Assertion
+{
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean isAllowed(AccessControlList acl, Role role, Resource resource, String permissionId)
+	{
+		if ("guest".equals(role.getRoleId()) && RoleImpl.class.isAssignableFrom(role.getClass()))
+		{
+			RoleImpl<String> principal = (RoleImpl<String>) role;
+
+			if (principal != null && OwnableResource.class.isAssignableFrom(resource.getClass()))
+			{
+				if (principal.getId().equals(((OwnableResource<String>) resource).getOwner()))
+				{
+					return acl.isAllowed("owner", resource.getResourceId(), permissionId);
+				}
+			}
+		}
+
+		return acl.isAllowed(role.getRoleId(), resource.getResourceId(), permissionId);
+	}
+}
