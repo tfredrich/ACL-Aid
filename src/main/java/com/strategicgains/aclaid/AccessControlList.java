64f8ed693a7c508a6f906c821fa633a0d2e3b76b
Todd Fredrich <tfredrich@gmail.com>
2022-03-17 17:02:35 -0600

    Javadoc changes.

--- src/main/java/com/strategicgains/aclaid/AccessControlList.java
+++ src/main/java/com/strategicgains/aclaid/AccessControlList.java
@@ -11,22 +11,52 @@ public class AccessControlList
 {
 	private Map<String, NamespaceConfiguration> namespaces = new HashMap<>();
 
+	/**
+	 * Get an existing NamespaceConfiguration by name or create a new, empty one.
+	 * Changes to the instance make changes to this AccessControlList.
+	 * 
+	 * @param namespace
+	 * @return an existing or new, empty NamespaceConfiguration instance.
+	 */
 	public NamespaceConfiguration namespace(String namespace)
 	{
 		return namespaces.computeIfAbsent(namespace, n -> new NamespaceConfiguration(this));
 	}
 
+	/**
+	 * Check to see if the given relation name is defined in any of the namespaces.
+	 * 
+	 * @param relation
+	 * @return
+	 */
 	public boolean containsRelation(String relation)
 	{
 		return namespaces.values().stream().anyMatch(n -> n.containsRelation(relation));
 	}
 
+	/**
+	 * Make an authorization check.
+	 * 
+	 * @param userset
+	 * @param relation
+	 * @param resource
+	 * @return
+	 * @throws ParseException
+	 */
 	public boolean check(String userset, String relation, String resource)
 	throws ParseException
 	{
 		return check(UserSet.parse(userset), relation, Resource.parse(resource));
 	}
 
+	/**
+	 * Make an authorization check.
+	 * 
+	 * @param userset
+	 * @param relation
+	 * @param resource
+	 * @return
+	 */
 	public boolean check(UserSet userset, String relation, Resource resource)
 	{
 		NamespaceConfiguration acl = namespaces.get(resource.getNamespace());
