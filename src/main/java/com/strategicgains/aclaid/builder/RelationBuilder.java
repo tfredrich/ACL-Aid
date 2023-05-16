0d5ddb286ff3f72280939c9414f653004e1ace2a
Todd Fredrich <tfredrich@gmail.com>
2023-05-16 15:59:45 -0600

    Removed policy usage CRUFT from RelationBuilder

--- src/main/java/com/strategicgains/aclaid/builder/RelationBuilder.java
+++ src/main/java/com/strategicgains/aclaid/builder/RelationBuilder.java
@@ -2,14 +2,12 @@ package com.strategicgains.aclaid.builder;
 
 import com.strategicgains.aclaid.NamespaceConfiguration;
 import com.strategicgains.aclaid.domain.Relation;
-import com.strategicgains.aclaid.policy.PolicyBuilder;
 
 public class RelationBuilder
 extends AbstractChildBuildable
 {
 	private String name;
 	private UsersetRewriteBuilder rewrites;
-	private PolicyBuilder policy;
 
 	public RelationBuilder(String relation, NamespaceConfigurationBuilder parent)
 	{
@@ -20,12 +18,6 @@ extends AbstractChildBuildable
 	public String getName()
 	{
 		return name;
-	}
-
-	public PolicyBuilder policy()
-	{
-		this.policy = new PolicyBuilder(this);
-		return policy;
 	}
 
 	public UsersetRewriteBuilder usersetRewrite()
@@ -43,17 +35,11 @@ extends AbstractChildBuildable
 	{
 		Relation r = new Relation(namespace, name);
 		if (hasRewrites()) rewrites.apply(r);
-		if (hasPolicy()) policy.apply(r);
 		return r;
 	}
 
 	private boolean hasRewrites()
 	{
 		return (rewrites != null);
-	}
-
-	private boolean hasPolicy()
-	{
-		return (policy != null);
 	}
 }
