2d4e222c8b8cd59cd5c446712f90367b2b3864c4
Todd Fredrich <tfredrich@gmail.com>
2024-03-19 17:11:55 -0600

    Whitespace and javadoc.

--- src/main/java/com/strategicgains/aclaid/domain/rewrite/RewriteRule.java
+++ src/main/java/com/strategicgains/aclaid/domain/rewrite/RewriteRule.java
@@ -7,8 +7,9 @@ import com.strategicgains.aclaid.domain.TupleSet;
 import com.strategicgains.aclaid.domain.UserSet;
 
 /**
- * In Zanzibar, Userset rewrite rules are defined per relation in a namespace. Each rule specifies
- * a function that takes an object ID as input and outputs a userset expression tree.
+ * In Zanzibar, Userset rewrite rules are defined per relation in a namespace.
+ * 
+ * Each rule specifies a function that takes an object ID as input and outputs a userset expression tree.
  * 
  * Userset rewrite rules are translated to boolean expressions as part of check evaluation.
  *
@@ -23,50 +24,54 @@ import com.strategicgains.aclaid.domain.UserSet;
  *     relation(inputObj, "viewer")
  * )
  *
- *	Outputs:
- *	       +--union--+
- *	      /           \
- *	     /             \
- *	    /               \
- *	intersection      viewer
- *	  /       \
- *	owner    editor
+ * Outputs:
+ *        +--union--+
+ *       /           \
+ *      /             \
+ *     /               \
+ * intersection      viewer
+ *   /       \
+ * owner    editor
  *
-
-union {
-   child { _this {} }
-   child { computed_userset { relation: "owner" } }
-}
-
-Outputs:
-     union
-    /     \
-_this     computed_userset
-                   \
-               relation: owner
-
-union {
-  child { _this {} }
-  child { computed_userset { relation: "editor" } }
-  child { tuple_to_userset {
-    tupleset { relation: "parent" }
-    computed_userset {
-      object: $TUPLE_USERSET_OBJECT  # parent folder
-      relation: "viewer"
-    }
- }}
-}
-
-Outputs:
-     +---------------+----------union---------------+
-    /               /                                \
-_this     computed_userset           +-----------tuple_to_userset---------+
-                   /                /                                      \
-          relation: editor     tupleset                             computed_userset
-                                   /                                /               \
-                            relation: parent     object: $TUPLE_USERSET_OBJECT     relation: viewer
-*/
+ * 
+ * union {
+ *    child { _this {} }
+ *    child { computed_userset { relation: "owner" } }
+ * }
+ * 
+ * Outputs:
+ *      union
+ *     /     \
+ * _this     computed_userset
+ *                    \
+ *                relation: owner
+ * 
+ * union {
+ *   child { _this {} }
+ *   child { computed_userset { relation: "editor" } }
+ *   child { tuple_to_userset {
+ *     tupleset { relation: "parent" }
+ *     computed_userset {
+ *       object: $TUPLE_USERSET_OBJECT  # parent folder
+ *       relation: "viewer"
+ *     }
+ *  }}
+ * }
+ * 
+ * Outputs:
+ *      +---------------+----------union---------------+
+ *     /               /                                \
+ * _this     computed_userset           +-----------tuple_to_userset---------+
+ *                    /                /                                      \
+ *           relation: editor     tupleset                             computed_userset
+ *                                    /                                /               \
+ *                             relation: parent     object: $TUPLE_USERSET_OBJECT     relation: viewer
+ */
 public interface RewriteRule
 {
+	// TODO: According to the Zanzibar paper, TupleSet is not the right return type. It should be UserSet, depending on the definition of 'userset' in this context.
+	//TupleSet rewrite(TupleSet tuples, ObjectId objectId);
+
+	// TODO: Even a Set<UserSet> is not the right return type. It should be a single UserSet if one believes the paper verbatim.
 	Set<UserSet> rewrite(TupleSet tuples, ObjectId objectId);
 }
